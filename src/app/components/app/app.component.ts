dc21879b79c9de0a16adc8cdaec43204f9916a7c
Wilomgfx <william@hugocantin.info>
2016-04-11 22:35:53 -0400

    gitignore fix

---
+++ src/app/components/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component }  from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'src/app/components/app/app.component.html',
+  styleUrls : ['src/app/components/app/app.component.css'],
+})
+
+
+export class AppComponent {
+  title = 'Yaecs - Yet an other e-commerce system!';
+}
